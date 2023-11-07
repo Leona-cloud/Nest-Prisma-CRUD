abd4f45ac1219e9de7cdc0a6db3b21056bfbd904
Leona-cloud <chisomokafor038@gmail.com>
2023-11-07 16:46:28 +0100

    chore: switch it up

--- src/casl/decorators/index.ts
+++ src/casl/decorators/index.ts
@@ -1,11 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 export const CHECK_ABILITY = 'check_ability';
 
-export interface RequiredRule {
-  action: string;
-  subject: string;
-  conditions?: any;
-}
+
 
 export const checkAbilities = (...requirements: RequiredRule[]) =>
   SetMetadata(CHECK_ABILITY, requirements);
